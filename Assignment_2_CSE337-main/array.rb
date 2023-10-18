62151d4091e07040f8b8c2c2605c0b717c29dbcc
lixingliu <lixing.liu@stonybrook.edu>
2023-10-18 19:22:57 -0400

    stuff

--- Assignment_2_CSE337-main/array.rb
+++ Assignment_2_CSE337-main/array.rb
@@ -9,7 +9,7 @@ class Array
         end
     end
 
-    def map(range = nil, &block)
+    def map(range = nil, &block)#DOUBLE CHECK WHAT INVALID RANGE IS
         if range == nil
             return self.original_map(&block)
         else
@@ -24,10 +24,10 @@ class Array
     end
 end
   
-# a = [1,2,34,5]
-# b = ["cat","bat","mat","sat"]
+a = [1,2,34,5]
+b = ["cat","bat","mat","sat"]
 # puts(b[-10], b[-9], b[-8], b[-7], b[-6], b[-5], b[-4], b[-3], b[-2], b[-1], b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7], b[8], b[9], b[10])
-# print b.map(-10..10) { |x| x[0].upcase + x[1,x.length] }, "\n"
+print b.map(-10..10) { |x| x[0].upcase + x[1,x.length] }, "\n"
 # print b.map(2..4) { |x| x[0].upcase + x[1,x.length] }, "\n"
 # print b.map { |x| x[0].upcase + x[1,x.length] }, "\n"
 # print b.map(2..10) { |x| x[0].upcase + x[1,x.length] }, "\n"
@@ -35,6 +35,7 @@ end
 # puts a[10]
 # print a.map(4..2) { |i| i.to_f}, "\n"
 # print a.map(2..4) { |i| i.to_f}, "\n"
+# print a.map(1..-3) {|i| i.to_f}, "\n"
 # print a.map { |i| i.to_f}, "\n"
 # puts b[-1]
 # puts b[5]
