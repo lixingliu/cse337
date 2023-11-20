cf35bd9238c1a8e1c6eedaea76a161ce2c82b0ec
lixingliu <lixing.liu@stonybrook.edu>
2023-11-20 15:07:22 -0500

    aa

--- Assignment_3_CSE337-main/prog4.py
+++ Assignment_3_CSE337-main/prog4.py
@@ -47,6 +47,29 @@ def on_remove_task(event = None):
     else:
         return
 
+def on_edit_task(event = None):
+    global task
+    global cal
+    cal.destroy()  # Destroy the existing calendar widget
+    window.update()
+    selected_indicies = active_list_box.curselection()
+    if selected_indicies:
+        index = selected_indicies[0]
+        selected_task = active_list_box.get(index)
+        task_name = selected_task.split(" - Priority: ")[0]  # Extract task name
+        priority_info = selected_task.split(" - Priority: ")[1]  # Extract priority and date information
+
+        priority = priority_info.split(" - Due Date: ")[0]  # Extract priority
+        due_date = priority_info.split(" - Due Date: ")[1]  # Extract due date
+        month, day, year = map(int, due_date.split('/'))
+        cal = Calendar(window, selectmode="day", year=year, month=month, day=day)
+        cal.place(x=10, y=10)  # Place the calendar at the top left
+        cal.bind("<<CalendarSelected>>", on_calendar_change)
+        entry.insert(0, task_name)
+        label.config(text=selected_task)  # Update the Label with the current text
+        print(selected_task)
+
+
 window = tk.Tk()
 window.title("To-Do List App")
 
@@ -63,6 +86,7 @@ combobox.bind("<<ComboboxSelected>>", on_combo_box_change)  # Bind event when co
 cal = Calendar(window)
 cal.pack(pady=10)
 cal.bind("<<CalendarSelected>>", on_calendar_change)  # Bind event when calendar date changes
+
 
 add_task_frame = tk.Frame(window)
 label = tk.Label(add_task_frame, text="To-Do Item")
@@ -90,12 +114,15 @@ task_name_frame.pack()
 active_list_box = tk.Listbox(window, selectmode=tk.SINGLE, width=150, height=20)
 mark_complete_button = tk.Button( text="Mark Complete", pady=10)
 mark_complete_button.bind("<Button-1>", on_complete_task)
+edit_task_button = tk.Button( text="Edit Task", pady=10)
+edit_task_button.bind("<Button-1>", on_edit_task)
 complete_list_box = tk.Listbox(window, selectmode=tk.SINGLE, width=150, height=10)
 remove_complete_button = tk.Button(text="Remove Completed", pady=10)
 remove_complete_button.bind("<Button-1>", on_remove_task)
 
 active_list_box.pack()
 mark_complete_button.pack()
+edit_task_button.pack()
 complete_list_box.pack()
 remove_complete_button.pack()
 
